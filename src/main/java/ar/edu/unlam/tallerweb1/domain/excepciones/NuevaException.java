1308073bc9579d9979855c1643796e954cbcfe06
MaglianoTomas <maglianotomas@gmail.com>
2023-07-02 05:51:59 -0300

    excepcion de prueba eliminada

--- src/main/java/ar/edu/unlam/tallerweb1/domain/excepciones/NuevaException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlam.tallerweb1.domain.excepciones;
-
-public class NuevaException extends RuntimeException{
-    public NuevaException(){
-        super("Tira error aca");
-    }
-}
